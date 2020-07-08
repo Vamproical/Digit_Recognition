7a5693357295d03b01f278f7e8b5ac9dd2f0ea32
Vamproical <mimoun136@yandex.ru>
2020-07-08 14:07:03 +0900

    Complete stage 2.

--- Digit Recognition/task/src/recognition/Main.java
+++ Digit Recognition/task/src/recognition/Main.java
@@ -3,30 +3,48 @@ package recognition;
 import java.util.Scanner;
 
 public class Main {
+    public static int findIndexOfMax(int[] numbers) {
+        int index = 0;
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > numbers[index]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] weights = new int[]{
-                2, 1, 2, 4, -4, 4, 2, -1, 2};
+        int[] bias = {-1, 6, 1, 0, 2, 0, -1, 3, -2, -1};
+        int[][] weights = {
+                {+1, +1, +1, +1, -1, +1, +1, -1, +1, +1, -1, +1, +1, +1, +1},//0
+                {-1, +1, -1, -1, +1, -1, -1, +1, -1, -1, +1, -1, -1, +1, -1},//1
+                {+1, +1, +1, -1, -1, +1, +1, +1, +1, +1, -1, -1, +1, +1, +1},//2
+                {+1, +1, +1, -1, -1, +1, +1, +1, +1, -1, -1, +1, +1, +1, +1},//3
+                {+1, -1, +1, +1, -1, +1, +1, +1, +1, -1, -1, +1, -1, -1, +1},//4
+                {+1, +1, +1, +1, -1, -1, +1, +1, +1, -1, -1, +1, +1, +1, +1},//5
+                {+1, +1, +1, +1, -1, -1, +1, +1, +1, +1, -1, +1, +1, +1, +1},//6
+                {+1, +1, +1, -1, -1, +1, -1, -1, +1, -1, -1, +1, -1, -1, +1},//7
+                {+1, +1, +1, +1, -1, +1, +1, +1, +1, +1, -1, +1, +1, +1, +1},//8
+                {+1, +1, +1, +1, -1, +1, +1, +1, +1, -1, -1, +1, +1, +1, +1},//9
+        };
         System.out.println("Input grid:");
-        String[] number = new String[9];
-        int k = 0;
-        while (k != 9) {
-            String[] numbers = scanner.nextLine().split("");
-            for (String s : numbers) {
-                number[k] = s;
-                ++k;
+        int[] number = new int[15];
+        for (int i = 0; i < 5; i++) {
+            char[] in = scanner.nextLine().toCharArray();
+            number[i * 3] = 'X' == in[0] ? 1 : 0;
+            number[i * 3 + 1] = 'X' == in[1] ? 1 : 0;
+            number[i * 3 + 2] = 'X' == in[2] ? 1 : 0;
+        }
+        int[] outputNumber = new int[10];
+        for (int i = 0; i < 10; i++) {
+            int result = bias[i];
+            for (int j = 0; j < 15; j++) {
+                result += number[j] * weights[i][j];
             }
+            outputNumber[i] = result;
         }
-        int result = -5;
-        for (int i = 0; i < 9; i++) {
-            if (number[i].equals("X")) {
-                result += weights[i];
-            }
-        }
-        if (result > 0) {
-            System.out.println("This number is 0");
-        } else {
-            System.out.println("This number is 1");
-        }
+        //System.out.println(Arrays.toString(outputNumber));
+        System.out.println("This number is " + findIndexOfMax(outputNumber));
     }
 }
